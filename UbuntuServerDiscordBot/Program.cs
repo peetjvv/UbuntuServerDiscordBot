363c3203fa0a32bbdd6c1e71f26ada2f96501c93
Peet Janse van Vuuren <peet@whereismytransport.com>
2018-12-28 14:59:43 +0200

    Add project files.

---
+++ UbuntuServerDiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UbuntuServerDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
