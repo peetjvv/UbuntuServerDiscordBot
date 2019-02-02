7c9bb95fb1eed4fd61113b2e26600e73adf59896
Peet Janse van Vuuren <peet@whereismytransport.com>
2019-02-02 16:51:54 +0200

    added the missing enum

---
+++ UbuntuServerDiscordBot/Enums/UbuntuProcessStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UbuntuServerDiscordBot.Enums
+{
+    public enum UbuntuProcessStatus
+    {
+        UninterruptibleSleep = 'D',
+        Running = 'R',
+        Sleeping = 'S',
+        StoppedByJobControlSignal = 'T',
+        StoppedByDebuggerDuringTrace = 't',
+        Zombie = 'Z'
+    }
+}
